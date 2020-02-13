f6e3b03a3ac9908b4f8bc161f8aa498b7c7229ce
An Nguyen <an.nguyenwasd@gmail.com>
2020-02-13 18:37:47 +0700

    feat: implement burgur menu

--- src/layout.js
+++ src/layout.js
@@ -1,43 +1,66 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components/macro";
+import { css, ThemeProvider } from "styled-components";
 import PropTypes from "prop-types";
 import logo from "assets/airbnb-logo.svg";
 import background from "assets/background.jpg";
 import { Menu, X } from "react-feather";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 function Layout({ children }) {
+  const [open, setOpen] = useState(false);
+  const location = useLocation();
+
+  useEffect(() => {
+    setOpen(false);
+  }, [location]);
+
   return (
-    <BackgroundedContainer>
-      <Container>
-        <Nav>
-          <Link to="/">
-            <img src={logo} alt="air bnb logo" />
-          </Link>
-          <NavControl>
-            <Menu />
-          </NavControl>
-          <Navs>
-            <li>dark/light</li>
-            <li>
-              <Link to="/host">Become a host</Link>
-            </li>
-            <li>
-              <Link to="/help">Help</Link>
-            </li>
-            <li>
-              <Link to="/sing-up">Sign up</Link>
-            </li>
-            <li>
-              <Link to="/login">Log in</Link>
-            </li>
-          </Navs>
-        </Nav>
-        {children}
-      </Container>
-    </BackgroundedContainer>
+    <ThemeProvider theme={{ open }}>
+      <BackgroundedContainer>
+        <Container>
+          <Nav>
+            <Link to="/">
+              <img src={logo} alt="air bnb logo" />
+            </Link>
+            <NavControl>
+              {open ? (
+                <X onClick={() => setOpen(false)} width={30} height={30} />
+              ) : (
+                <Menu onClick={() => setOpen(true)} width={30} height={30} />
+              )}
+            </NavControl>
+            <Navs open={open}>
+              <li>dark/light</li>
+              <li>
+                <Link to="/host">Become a host</Link>
+              </li>
+              <li>
+                <Link to="/help">Help</Link>
+              </li>
+              <li>
+                <Link to="/sing-up">Sign up</Link>
+              </li>
+              <li>
+                <Link to="/login">Log in</Link>
+              </li>
+            </Navs>
+          </Nav>
+          <Content>{children}</Content>
+        </Container>
+      </BackgroundedContainer>
+    </ThemeProvider>
   );
 }
+
+// hold position for nav
+const Content = styled.main`
+  ${p =>
+    p.theme.open &&
+    css`
+      padding-top: 60px;
+    `};
+`;
 
 const Nav = styled.nav`
   display: flex;
@@ -45,6 +68,22 @@ const Nav = styled.nav`
   justify-content: space-between;
   padding: 24px 29px 0;
   color: white;
+  height: 60px;
+
+  @media screen and (max-width: 769px) {
+    ${p =>
+      p.theme.open &&
+      css`
+        height: 100vh;
+        background-color: rgba(71, 71, 71, 0.65);
+        align-items: flex-start;
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100vw;
+        z-index: 2;
+      `};
+  }
 `;
 
 const NavControl = styled.div`
@@ -63,7 +102,17 @@ const Navs = styled.ul`
   line-height: 16px;
 
   @media screen and (max-width: 769px) {
-    display: none;
+    display: ${p => (p.open ? "block" : "none")};
+    position: absolute;
+    top: 90px;
+    padding: 0;
+    margin: 0;
+
+    li {
+      margin: 0 !important;
+      padding: 10px 0;
+      font-size: 16px;
+    }
   }
 
   color: #ffffff;
@@ -81,6 +130,7 @@ const Navs = styled.ul`
 const Container = styled.div`
   max-width: 1440px;
   margin: 0 auto;
+  position: relative;
 `;
 
 const BackgroundedContainer = styled.div`
